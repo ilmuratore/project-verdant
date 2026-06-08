5db86f4fb5d2e7da8560f1ee7618fbddd7f5d4a6
Simone Iengo <iengo.simone@gmail.com>
2026-06-08 14:57:44 +0200

    lesson 5: first attributes, damage change, health system, first player ui

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+    public TMP_Text healthText;
+    public Animator healthTextAnim;
+
+    private void Start()
+    {
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+        healthTextAnim.Play("TextAnimation");
+        healthText.text = "HP: " + currentHealth + "/ " + maxHealth;
+        if ( currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+}
